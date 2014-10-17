7e9f6b11ed573e74d34157bd8ef8602442d2d840
aluuu <aluuu@husa.su>
2014-10-17 11:00:30 +0300

    complete node parsing test

--- test/test_parser.ml
+++ test/test_parser.ml
@@ -12,12 +12,20 @@ let create_xml_input str =
 
 let test_parse_node () =
   let xml_input = create_xml_input node_example in
-  let Some (OSMNode node) = Parser.parse_node xml_input in
-  assert_equal node.id (OSMId "25496583");
-  assert_equal node.user "80n";
-  assert_equal node.visible true;
-  assert_equal node.latitude 51.5173639;
-  assert_equal node.longitude (-. 0.140043)
+  let result = Parser.parse_node xml_input in
+  match result with
+  | Some (OSMNode node) ->
+     assert_equal node.id (OSMId "25496583");
+     assert_equal node.user "80n";
+     assert_equal node.visible true;
+     assert_equal node.latitude 51.5173639;
+     assert_equal node.longitude (-. 0.140043);
+     assert_equal (find_tag node.tags "highway")
+                  (Some "traffic_signals")
+  | Some _ ->
+     failwith "Wrong parser was given for this fixture.
+               Maybe, `parse_way` or `parse_relation` was used."
+  | None -> failwith "Wrong fixture was given to parse_node."
 
 let test_parse_way () =
   let xml_input = create_xml_input way_example in
